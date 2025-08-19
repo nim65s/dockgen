c67b515675b7df6ed0b98b4129aca42bbc5a34f2
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-08-19 15:34:40 +0200

    release.sh: no need to bump pyproject.toml

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,6 @@ uv version --bump $1
 NEW=$(uv version --short)
 DATE=$(date +%Y-%m-%d)
 
-sed -i "/^version =/s/$OLD/$NEW/" pyproject.toml
 sed -i "/^## \[Unreleased\]/a \\\n## [v$NEW] - $DATE" CHANGELOG.md
 sed -i "/^\[Unreleased\]/s/$OLD/$NEW/" CHANGELOG.md
 sed -i "/^\[Unreleased\]/a [v$NEW]: https://github.com/nim65s/dockgen/compare/v$OLD...v$NEW" CHANGELOG.md
